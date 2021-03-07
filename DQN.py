2227ff0f047768ee70627e6fe13bce49411e3cbd
ggpen <ggpent2@gmail.com>
2021-03-07 11:26:08 +0200

    memory class added to dqn

--- DQN.py
+++ DQN.py
@@ -1,9 +1,9 @@
 # this code will show the simple dqn model
 import torch.nn as nn
-import torch as T
+import torch 
 import torch.optim as optim
 from torch.nn import MSELoss
-
+import numpy as np
 
 class DQN(nn.Module):
     def __init__(self, input_shape, num_actions, lr=0.001):
@@ -18,8 +18,8 @@ class DQN(nn.Module):
 
         self.fc1 = nn.Linear(self.feature_size(), 512)
         self.fc2 = nn.Linear(512, self.num_actions)
-        self.loss = nn.MSELoss()
-        self.optim = torch.optim.RMSprop(self.parameters(),lr = lr)
+        self.loss = MSELoss()
+        self.optim = optim.RMSprop(self.parameters(),lr = lr)
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.to(self.device)
         self.name = name
@@ -36,3 +36,41 @@ class DQN(nn.Module):
     
     def feature_size(self):
         return self.conv3(self.conv2(self.conv1(torch.zeros(1, *self.input_shape)))).view(1, -1).size(1)
+
+'''
+    so why do we need memory for our agent?
+    of course the answer for this is because we cant know what happends in the game by just one frame we need more frames.
+    if we take batch_size number of frames.
+    for example 4 so we can see the direction of the agent and the envirment,
+    whice can help figure out what is the best action to do
+'''
+class Memory:
+    def __init__(self, mem_size, in_dims):
+        self.mem_size = mem_size
+        self.mem_counter = 0
+        self.state_memory = np.zeros((mem_size, in_dims), dtype=np.float32)
+        self.next_state_memory = np.zeros((mem_size, in_dims), dtype=np.float32)
+        self.action_memory = np.zeros((mem_size), dtype=np.int64)
+        self.reward_memory = np.zeros((mem_size), dtype=np.int64)
+        self.done_memory = np.zeros((mem_size), dtype=np.uint8)
+
+    def store_memory(self, state, action, reward, state_, done):
+        indx = self.mem_counter%self.mem_size
+        self.state_memory[indx] = state
+        self.next_state_memory[indx] = state_
+        self.action_memory[indx] = action
+        self.reward_memory[indx] = reward
+        self.done_memory[indx] = done
+    
+    def sample_memory(self, batch_size):
+        if self.mem_counter >= batch_size:
+            if self.mem_counter >= self.mem_size:
+                indx = np.random.choice(self.mem_size, batch_size, replace=False)
+            else:
+                indx = np.random.choice(self.mem_counter, batch_size, replace=False)
+            state = self.state_memory[indx]
+            action = self.action_memory[indx]
+            reward = self.reward_memory[indx]
+            state_ = self.next_state_memory[indx]
+            done = self.done_memory[indx]
+            return state, aciton, reward, state_, done
